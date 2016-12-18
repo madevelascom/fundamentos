ba9f8ca87897c42884dc45769ddfb3ecb8353000
mvelasco93 <m_velasco93@hotmail.com>
2016-12-18 03:03:44 -0500

    Menu links

--- application/views/frontend/menu.php
+++ application/views/frontend/menu.php
@@ -13,7 +13,7 @@
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="web/index">Inicio <span class="sr-only">(current)</span></a></li>
+				<li class="active"><a href="<?php echo site_url("web/index"); ?>">Inicio <span class="sr-only">(current)</span></a></li>
 				<li><a href="<?php echo site_url("web/semanas"); ?>">Curso</a></li>
 				<li><a href="<?php echo site_url("web/semanas"); ?>">Semana a Semana</a></li>
 				<li><a href="<?php echo site_url("web/ayudantias"); ?>">Ayudantías</a></li>
